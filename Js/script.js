71241a2b0dcc1cc1b31d4f37a242514d693a18dd
Dennis C. Mathews Jr. <dmathewsjr07106@gmail.com>
2024-04-24 23:23:45 -0400

    Files Updated

--- Js/script.js
+++ Js/script.js
@@ -9,18 +9,47 @@ topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
 topMenuEl.style.height = '100%';
 topMenuEl.className = 'flex-around';
 
+const subMenuEl = document.getElementById('sub-menu');
+subMenuEl.style.height = '100%';
+subMenuEl.style.backgroundColor = 'var(--sub-menu-bg)';
+subMenuEl.className = 'flex-around';
+subMenuEl.style.position = absolute;
+subMenuEl.style.top = '0';
 
-const menuLinks = [
-    { text: 'about', href: '/about' },
-    { text: 'catalog', href: '/catalog' },
-    { text: 'orders', href: '/orders' },
-    { text: 'account', href: '/account' },
-  ];
 
+let menuLinks = [
+  {text: 'about', href: '/about'},
+  {text: 'catalog', href: '#', subLinks: [
+    {text: 'all', href: '/catalog/all'},
+    {text: 'top selling', href: '/catalog/top'},
+    {text: 'search', href: '/catalog/search'},
+  ]},
+  {text: 'orders', href: '#' , subLinks: [
+    {text: 'new', href: '/orders/new'},
+    {text: 'pending', href: '/orders/pending'},
+    {text: 'history', href: '/orders/history'},
+  ]},
+  {text: 'account', href: '#', subLinks: [
+    {text: 'profile', href: '/account/profile'},
+    {text: 'sign out', href: '/account/signout'},
+  ]},
+];
+  
   for (let i = 0; i < menuLinks.length; i++) {
     const a = document.createElement('a');
-    a.setAttribute('href',menuLinks[i].href);
+    a.setAttribute('href', menuLinks[i].href);
     a.textContent = menuLinks[i].text;
      topMenuEl.appendChild(a);
+   
+  } 
+  const topMenuLinks = topMenuEl.querySelectorAll('a');
+  topMenuEl.addEventListener('click', function callback (e) {
+
     
-  }
+        e.preventDefault();
+        
+    return e !== a;
+    console.log(a);
+
+    
+  });
